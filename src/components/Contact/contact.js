ce36a0a8a006bb57723c0fd6fa5e72356999e1ac
victor <onyejekwev@gmail.com>
2024-08-28 18:41:04 -0700

    Working on contacts

---
+++ src/components/Contact/contact.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import './contact.css'
+import { RiMessengerLine } from "react-icons/ri";
+import { BsWhatsapp } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
+import {useRef} from 'react';
+import emailjs from '@emailjs/browser';
+
+
+
+
+const Contact = () => {
+  
+const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_sdtdxpj', 'template_sxtfyml', form.current, {
+        publicKey: 'IGGTamXk_XBi56-8_',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          alert('Your Message was sucessfuly sent,Thanks for Reaching out.')
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+          alert('Your Message was not sent,Something went wrong.')
+
+        },
+      );
+
+      e.target.reset();
+  };
+}
+
+export default Contact;
