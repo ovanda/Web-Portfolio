eb726d3971a357fd0718e04eac5f7e25ac96f99c
victor <onyejekwev@gmail.com>
2024-08-29 03:29:28 -0700

    Upgrading my portfolio site

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -14,7 +14,7 @@ const About = () => {
              <p>As a skilled web designer, i craft an exceptional digital experience 
                 that captivate audiences and drive results. With keen eyes for details 
                 and a passion for innovation, i create visually stunning and intuitive websites
-                that engage brands and inspire engagement. I am proficient in
+                that elevate brands and inspire engagement. I am proficient in
                 HTML, CSS, Javascript,React js,Express js,Node js & PostgreSql.
              </p>
               
